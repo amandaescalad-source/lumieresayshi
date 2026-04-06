f035464455a96d8f82e56b9e72bbb49f9bfd75bc
amandaescalad-source <amanda.escalad@gmail.com>
2026-04-06 11:56:57 +0200

    Update load-content.js

--- netlify/functions/load-content.js
+++ netlify/functions/load-content.js
@@ -1,28 +1,7 @@
-const { getStore } = require('@netlify/blobs');
-
-exports.handler = async (event) => {
-  try {
-    const store = getStore('studio-lumiere');
-    const data = await store.get('site-content', { type: 'json' });
-
-    if (!data) {
-      return {
-        statusCode: 200,
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({})
-      };
-    }
-
-    return {
-      statusCode: 200,
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data)
-    };
-  } catch (err) {
-    return {
-      statusCode: 200,
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({})
-    };
-  }
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({})
+  };
 };
