8a7576a2232cc0a18832b8d636cc1e1fd2f945dc
amandaescalad-source <amanda.escalad@gmail.com>
2026-04-06 11:56:37 +0200

    Update save-content.js

--- netlify/functions/save-content.js
+++ netlify/functions/save-content.js
@@ -1,15 +1,9 @@
-const { getStore } = require('@netlify/blobs');
-
 exports.handler = async (event) => {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' };
   }
-
   try {
     const data = JSON.parse(event.body);
-    const store = getStore('studio-lumiere');
-    await store.setJSON('site-content', data);
-
     return {
       statusCode: 200,
       headers: { 'Content-Type': 'application/json' },
